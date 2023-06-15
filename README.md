e94d3bbdf2961cb31b45858036ec9c06aa62adb1
Bert Tijskens <engelbert.tijskens@uantwerpen.be>
2023-06-15 14:41:20 +0200

    wip

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # cookiecutter-wip-project
+
+Part of the CLI github.com/etijskens/wetppr/wetppr/cli/wip.py.
+
+This cookiecutter sets up a new Python project. 
--- {{cookiecutter.project_name}}/tests/{{cookiecutter.package_name}}/test_{{cookiecutter.package_name}}.py
+++ {{cookiecutter.project_name}}/tests/{{cookiecutter.package_name}}/test_{{cookiecutter.package_name}}.py
@@ -1,23 +1,21 @@
 # -*- coding: utf-8 -*-
 
-"""Tests for {{tmpl.package_name}} package."""
+"""Tests for {{cookiecutter.package_name}} package."""
 
 import sys
 sys.path.insert(0,'.')
 
-import {{tmpl.package_name}}
+import {{cookiecutter.package_name}}
 
 
-def test_hello_noargs():
-    """Test for {{tmpl.package_name}}.hello()."""
-    s = {{tmpl.package_name}}.hello()
-    assert s == "Hello world"
+def test_hello_default_arg():
+    result = {{cookiecutter.package_name}}.hello()
+    assert result == "Hello world!"
 
 
 def test_hello_me():
-    """Test for {{tmpl.package_name}}.hello('me')."""
-    s = {{tmpl.package_name}}.hello('me')
-    assert s == "Hello me"
+    result = {{cookiecutter.package_name}}.hello('me')
+    assert result == "Hello me!"
 
 
 # ==============================================================================
@@ -27,7 +25,7 @@ def test_hello_me():
 # that the source directory is on the path
 # ==============================================================================
 if __name__ == "__main__":
-    the_test_you_want_to_debug = test_hello_noargs
+    the_test_you_want_to_debug = test_hello_default_arg
 
     print("__main__ running", the_test_you_want_to_debug)
     the_test_you_want_to_debug()
